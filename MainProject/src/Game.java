4fd601fc10ec31e24a8862144df5d856d76f6652
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2025-08-17 23:13:31 +0100

    Button interaction and panel inclusion
    Implemented the increase and decrease buttons alongside their interactions through action listeners which, currently, only modify the number through singular and rapid clicks.
    
    Also added a JPanel which will serve as a child container of the JFrame for all elements such as buttons and labels.

--- MainProject/src/Game.java
+++ MainProject/src/Game.java
@@ -1,26 +1,63 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game {
     //region UI
     private JFrame screen;
+    private JPanel panel;
     private JLabel numberLabel;
     private Font generalFont;
+    private JButton increaseBtn, decreaseBtn;
     //endregion
+
+    private int number;
 
     public static void main(String[] args) {
         Game game = new Game();
-        game.showScreen();
+        game.setupUI();
+        game.setupInteraction();
     }
 
-    public void showScreen(){
+    public void setupUI() {
         generalFont = new Font(Font.SANS_SERIF, Font.PLAIN, 30);
         screen = new JFrame("Number Clicker");
         screen.setSize(800, 600);
         screen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        screen.setLocationRelativeTo(null);
         screen.setVisible(true);
-        numberLabel = new JLabel("0", SwingConstants.CENTER);
+
+        panel = new JPanel();
+        screen.add(panel, BorderLayout.CENTER);
+
+        increaseBtn = new JButton("Increase");
+        increaseBtn.setFont(generalFont);
+        decreaseBtn = new JButton("Decrease");
+        decreaseBtn.setFont(generalFont);
+        panel.add(increaseBtn);
+        panel.add(decreaseBtn);
+
+        numberLabel = new JLabel(String.valueOf(number), SwingConstants.CENTER);
         numberLabel.setFont(generalFont);
-        screen.add(numberLabel);
+        panel.add(numberLabel);
+    }
+
+    public void setupInteraction() {
+        increaseBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                number++;
+                numberLabel.setText(String.valueOf(number));
+            }
+        });
+
+        decreaseBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                number--;
+                numberLabel.setText(String.valueOf(number));
+            }
+        });
     }
 }
