819ab976e252838fa27de1384d7b69b712167da7
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2025-08-18 16:06:28 +0100

    Better UI layout and button hold
    Added separate JPanels for the number label and buttons to allow for better positioning and size of the elements as there is also dedicated fonts for them (with the number font being bigger to make it be the primary focus on screen).
    
    Also implemented a way for the buttons to be held down with the mouse which can increase/decrease the number consecutively through the usage of MouseAdapters and Timers.
    
    The MouseAdapters includes helpful overridable methods which are invoked depending on the mouses action with the button such as mousePressed and mouseReleased whilst the timer allows for multiple mouse events to be fired.

--- MainProject/src/Game.java
+++ MainProject/src/Game.java
@@ -2,17 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Game {
     //region UI
     private JFrame screen;
-    private JPanel panel;
+    private JPanel numberPanel, btnPanel;
     private JLabel numberLabel;
-    private Font generalFont;
+    private Font btnFont, numberFont;
     private JButton increaseBtn, decreaseBtn;
     //endregion
 
-    private int number;
+    private int number = 0;
 
     public static void main(String[] args) {
         Game game = new Game();
@@ -21,30 +23,46 @@ public class Game {
     }
 
     public void setupUI() {
-        generalFont = new Font(Font.SANS_SERIF, Font.PLAIN, 30);
+        btnFont = new Font(Font.SANS_SERIF, Font.PLAIN, 45);
+        numberFont = new Font(Font.SANS_SERIF, Font.BOLD, 120);
+
+        //Creating the game window
         screen = new JFrame("Number Clicker");
         screen.setSize(800, 600);
         screen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         screen.setLocationRelativeTo(null);
         screen.setVisible(true);
 
-        panel = new JPanel();
-        screen.add(panel, BorderLayout.CENTER);
+        //Panel setup
+        btnPanel = new JPanel();
+        btnPanel.setBounds(100, 250, 600, 200);
+        numberPanel = new JPanel();
+        numberPanel.setBounds(700, 500, 100, 100);
+        screen.add(btnPanel, BorderLayout.CENTER);
+        screen.add(numberPanel, BorderLayout.CENTER);
 
+        //Btn setup
         increaseBtn = new JButton("Increase");
-        increaseBtn.setFont(generalFont);
-        decreaseBtn = new JButton("Decrease");
-        decreaseBtn.setFont(generalFont);
-        panel.add(increaseBtn);
-        panel.add(decreaseBtn);
+        increaseBtn.setFont(btnFont);
+        increaseBtn.setMargin(new Insets(20, 20, 20, 20));
+        increaseBtn.setFocusPainted(false);
 
+        decreaseBtn = new JButton("Decrease");
+        decreaseBtn.setFont(btnFont);
+        decreaseBtn.setMargin(new Insets(20, 20, 20, 20));
+        decreaseBtn.setFocusPainted(false);
+
+        btnPanel.add(increaseBtn);
+        btnPanel.add(decreaseBtn);
+
+        //Label setup
         numberLabel = new JLabel(String.valueOf(number), SwingConstants.CENTER);
-        numberLabel.setFont(generalFont);
-        panel.add(numberLabel);
+        numberLabel.setFont(numberFont);
+        numberPanel.add(numberLabel);
     }
 
     public void setupInteraction() {
-        increaseBtn.addActionListener(new ActionListener() {
+        Timer increaseBtnTimer = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 number++;
@@ -52,11 +70,65 @@ public class Game {
             }
         });
 
-        decreaseBtn.addActionListener(new ActionListener() {
+        increaseBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                number++;
+                numberLabel.setText(String.valueOf(number));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                if (!increaseBtnTimer.isRunning()) {
+                    increaseBtnTimer.start();
+                    increaseBtnTimer.setDelay(20);
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                {
+                    increaseBtnTimer.setDelay(100);
+                    increaseBtnTimer.stop();
+                }
+            }
+        });
+
+        Timer decreaseBtnTimer = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 number--;
                 numberLabel.setText(String.valueOf(number));
+            }
+        });
+
+        decreaseBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                number--;
+                numberLabel.setText(String.valueOf(number));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                if (!decreaseBtnTimer.isRunning()) {
+                    decreaseBtnTimer.start();
+                    decreaseBtnTimer.setDelay(20);
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                {
+                    decreaseBtnTimer.setDelay(100);
+                    decreaseBtnTimer.stop();
+                }
             }
         });
     }
